9d3f6d855455405433794f233e4e184f163201a4
Fathul <97374618+Himss2@users.noreply.github.com>
2026-07-28 07:20:55 +0700

    Create Config.hpp

---
+++ include/Core/Config.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace config {
+
+struct Settings {
+    int zoomAnimSpeed = 5;          // Range 1 (Lambat) - 10 (Cepat)
+    bool enableSpyglassSound = true; // Toggle Suara Spyglass
+    bool hideHandOnZoom = true;      // Toggle Sembunyikan Tangan saat Zoom
+};
+
+extern Settings g_settings;
+
+void Load();
+void Save();
+void RegisterModMenu();
+
+} // namespace config
