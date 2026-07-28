e67a11a6d6b0b8e9102bf3da0c6481ee0987122d
Fathul <97374618+Himss2@users.noreply.github.com>
2026-07-28 11:12:00 +0700

    Update Config.cpp

--- src/Core/Config.cpp
+++ src/Core/Config.cpp
@@ -4,26 +4,6 @@
 #include <nlohmann/json.hpp>
 #include <fstream>
 
-// Deteksi otomatis lokasi header Mod Menu di preloader-android
-#if __has_include(<pl/ModMenu.hpp>)
-    #include <pl/ModMenu.hpp>
-    #define HAS_MODMENU 1
-#elif __has_include(<pl/ModMenuBridge.hpp>)
-    #include <pl/ModMenuBridge.hpp>
-    #define HAS_MODMENU 1
-#elif __has_include(<pl/runtime/ModMenu.hpp>)
-    #include <pl/runtime/ModMenu.hpp>
-    #define HAS_MODMENU 1
-#elif __has_include(<pl/runtime/ModMenuBridge.hpp>)
-    #include <pl/runtime/ModMenuBridge.hpp>
-    #define HAS_MODMENU 1
-#elif __has_include(<pl/modmenu/ModMenu.hpp>)
-    #include <pl/modmenu/ModMenu.hpp>
-    #define HAS_MODMENU 1
-#else
-    #define HAS_MODMENU 0
-#endif
-
 namespace config {
 
 Settings g_settings;
@@ -43,6 +23,8 @@ void Load() {
         if (j.contains("posX")) g_settings.posX = j["posX"];
         if (j.contains("posY")) g_settings.posY = j["posY"];
         if (j.contains("scale")) g_settings.scale = j["scale"];
+        
+        core::Log().info("Config: Loaded successfully from file");
     } catch (...) {
         core::Log().warn("Config: Failed to parse settings file, using defaults");
     }
@@ -63,26 +45,8 @@ void Save() {
 }
 
 void RegisterModMenu() {
-#if HAS_MODMENU
-    using namespace pl::modmenu;
-
-    auto builder = ModuleBuilder("SmoothZoom", "Smooth Zoom with Drag Controls");
-    
-    builder.config("zoomAnimSpeed", "Kecepatan Animasi", ConfigType::Slider, "1,10,1", std::to_string(g_settings.zoomAnimSpeed))
-           .config("hideHandOnZoom", "Sembunyikan Tangan", ConfigType::Radio, "0", g_settings.hideHandOnZoom ? "1" : "0")
-           .onConfigChanged([](const std::string& key, const std::string& value) {
-               if (key == "zoomAnimSpeed") {
-                   g_settings.zoomAnimSpeed = std::stoi(value);
-               } else if (key == "hideHandOnZoom") {
-                   g_settings.hideHandOnZoom = (value == "1");
-               }
-               Save();
-           });
-
-    RegisterModule(builder.build());
-#else
-    core::Log().info("Config: ModMenu header tidak ditemukan pada build ini, melewati registrasi ModMenu GUI.");
-#endif
+    // Dipanggil saat mod initialize untuk membaca config
+    Load();
 }
 
 } // namespace config
